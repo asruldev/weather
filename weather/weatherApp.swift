ca3ed4d6653ea353329c330e1e75cba0787682ed
Asrul Harahap <admin@asrul.dev>
2024-06-22 21:48:48 +0700

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Asrul Harahap on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
